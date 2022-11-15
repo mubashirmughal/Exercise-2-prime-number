67c828de2b549db271825da5c0efb2a820c12bd1
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-14 19:11:18 -0500

    gjkn

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,39 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        int a , b , f1;
+class gfg {
 
-        System.out.println("Hello world!");
-        System.out.println("All the Prime numbers within 1 and " + N + " are:");
-        
+    static void prime_N(int N)
+    {
+        int x, y, flg;
+
+        System.out.println(
+                "All the Prime numbers within 1 and " + N
+                        + " are:");
+
+        for (x = 1; x <= N; x++) {
+
+            if (x == 1 || x == 0)
+                continue;
+
+            flg = 1;
+
+            for (y = 2; y <= x / 2; ++y) {
+                if (x % y == 0) {
+                    flg = 0;
+                    break;
+                }
+            }
+            if (flg == 1)
+                System.out.print(x + " ");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int N = sc.nextInt();
+
+        prime_N(N);
     }
 }
