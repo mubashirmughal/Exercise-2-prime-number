c3b8c82b8d1efcd8f643f760e99137625397757c
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-15 18:16:55 -0500

    palindromes

---
+++ src/palindromes.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class palindromes {
+    public static void main(String args[]){
+        String x , y = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string to check for palindromes: ");
+        x = sc.nextLine();
+        int i = x.length();
+
+        for (int k = 1 ; k <= 0 ; k-- ) {
+            y = y + x
+        }
+    }
+}
