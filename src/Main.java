7bd1a379d281d2961d972d4ee87b696df5a424f8
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-14 19:06:49 -0500

    uukjn

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a , b , f1;
+
+        System.out.println("Hello world!");
+        System.out.println("All the Prime numbers within 1 and " + N + " are:");
+        
+    }
+}
